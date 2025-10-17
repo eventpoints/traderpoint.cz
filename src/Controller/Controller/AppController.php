6d3f8232ee6ada7fc28db1898143e1ca240d78f4
kez <kerrialbeckettnewham@gmail.com>
2025-10-17 13:59:31 +0200

    fix cs

--- src/Controller/Controller/AppController.php
+++ src/Controller/Controller/AppController.php
@@ -3,12 +3,10 @@
 namespace App\Controller\Controller;
 
 use App\Entity\User;
-use Carbon\CarbonImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class AppController extends AbstractController
