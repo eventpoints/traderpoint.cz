656f97efc5514bd97577df7b4c4c700bc3b0227b
kez <kerrialbeckettnewham@gmail.com>
2025-10-17 13:59:05 +0200

    add metatags | add sitemap

--- src/Controller/Controller/AppController.php
+++ src/Controller/Controller/AppController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Controller;
 
 use App\Entity\User;
+use Carbon\CarbonImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class AppController extends AbstractController
