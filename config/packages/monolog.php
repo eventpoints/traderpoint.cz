c0306c073853f21095b52287aa818cb2266c5faf
kez <kerrialbeckettnewham@gmail.com>
2025-10-19 15:24:04 +0200

    fix logging

--- config/packages/monolog.php
+++ config/packages/monolog.php
@@ -6,56 +6,9 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('monolog', [
-        'channels' => [
-            'deprecation',
-        ],
+        'channels' => ['deprecation', 'upload'],
     ]);
-    if ($containerConfigurator->env() === 'dev') {
-        $containerConfigurator->extension('monolog', [
-            'handlers' => [
-                'main' => [
-                    'type' => 'stream',
-                    'path' => '%kernel.logs_dir%/%kernel.environment%.log',
-                    'level' => 'debug',
-                    'channels' => [
-                        '!event',
-                    ],
-                ],
-                'console' => [
-                    'type' => 'console',
-                    'process_psr_3_messages' => false,
-                    'channels' => [
-                        '!event',
-                        '!doctrine',
-                        '!console',
-                    ],
-                ],
-            ],
-        ]);
-    }
-    if ($containerConfigurator->env() === 'test') {
-        $containerConfigurator->extension('monolog', [
-            'handlers' => [
-                'main' => [
-                    'type' => 'fingers_crossed',
-                    'action_level' => 'error',
-                    'handler' => 'nested',
-                    'excluded_http_codes' => [
-                        404,
-                        405,
-                    ],
-                    'channels' => [
-                        '!event',
-                    ],
-                ],
-                'nested' => [
-                    'type' => 'stream',
-                    'path' => '%kernel.logs_dir%/%kernel.environment%.log',
-                    'level' => 'debug',
-                ],
-            ],
-        ]);
-    }
+
     if ($containerConfigurator->env() === 'prod') {
         $containerConfigurator->extension('monolog', [
             'handlers' => [
@@ -63,10 +16,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                     'type' => 'fingers_crossed',
                     'action_level' => 'error',
                     'handler' => 'nested',
-                    'excluded_http_codes' => [
-                        404,
-                        405,
-                    ],
+                    'excluded_http_codes' => [404, 405],
                     'buffer_size' => 50,
                 ],
                 'nested' => [
@@ -78,17 +28,19 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 'console' => [
                     'type' => 'console',
                     'process_psr_3_messages' => false,
-                    'channels' => [
-                        '!event',
-                        '!doctrine',
-                    ],
+                    'channels' => ['!event', '!doctrine'],
                 ],
                 'deprecation' => [
                     'type' => 'stream',
-                    'channels' => [
-                        'deprecation',
-                    ],
+                    'channels' => ['deprecation'],
                     'path' => 'php://stderr',
+                    'formatter' => 'monolog.formatter.json',
+                ],
+                'app_info' => [
+                    'type' => 'stream',
+                    'path' => 'php://stdout',
+                    'level' => 'info',
+                    'channels' => ['app'],
                     'formatter' => 'monolog.formatter.json',
                 ],
             ],
