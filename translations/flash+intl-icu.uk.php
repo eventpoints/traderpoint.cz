c44dafe8798c7c21edd2d15578f9f19399c53729
kez <kerrialbeckettnewham@gmail.com>
2025-11-30 16:44:50 +0100

    fix translations

--- translations/flash+intl-icu.uk.php
+++ translations/flash+intl-icu.uk.php
@@ -5,11 +5,11 @@ return array(
     'flash.payment.expired' => 'Строк дії платежу минув',
     'flash.payment.failed' => 'Помилка оплати',
     'flash.payment.pending' => 'Платіж очікується',
-    'flash.quote-sent-successful' => 'Кошторис успішно надіслано',
+    'flash.quote-sent-successful' => 'Цінову пропозицію успішно надіслано',
     'flash.review.created' => 'Відгук надіслано',
     'flash.sceptical-issue' => 'Хмм, щось пішло не так',
     'flash.email-address-confirmed' => 'Електронну адресу підтверджено',
     'flash.password-changed' => 'Пароль успішно змінено',
-    'flash.engagement-deleted' => 'завдання видалено',
+    'flash.engagement-deleted' => 'Оголошення видалено',
 
 );
